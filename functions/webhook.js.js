63f0708819f85f0e60665f19a2e5b13c961fe52f
LaxmanSingh9 <laxmansisodiya77846@gmail.com>
2023-06-22 21:49:33 +0530

    first commit

---
+++ functions/webhook.js.js
@@ -0,0 +1,31 @@
+// functions/webhook.js
+
+const admin = require('firebase-admin');
+
+// Use a service account
+const serviceAccount = require('./serviceAccountKey.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://your-database-url.firebaseio.com'
+});
+
+const db = admin.database();
+
+exports.handler = async function(event, context) {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+
+  const data = JSON.parse(event.body);
+
+  // Get a reference to the database
+  const ref = db.ref('webhook_data');
+
+  // Push data to the database
+  await ref.push(data);
+
+  return {
+    statusCode: 200,
+    body: 'OK'
+  };
+};
